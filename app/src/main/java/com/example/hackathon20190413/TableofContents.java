87595efb3000539abc58d3feaf2c2605a27b0d73
echosierra1 <2010els@gmail.com>
2019-04-13 15:38:00 -0500

    Updated for Authorization and Updates, GPA Caluculator

---
+++ app/src/main/java/com/example/hackathon20190413/TableofContents.java
@@ -0,0 +1,27 @@
+package com.example.hackathon20190413;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TableofContents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tableof_contents);
+    }
+
+    public void onGPAbuttonClicked(View v) {
+        Intent i = new Intent(this, InputGPA.class);
+        startActivity(i);
+    }
+
+    public void onUpdateButtonPress(View v) {
+        Intent i = new Intent(this, updateUser.class);
+        startActivity(i);
+    }
+
+
+}
